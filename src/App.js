7ab660d239aaff7403b15a2b87b23636111f2c09
Franco Settembrini <fransettembrini@gmail.com>
2021-10-09 16:05:09 -0300

    NavBar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { NavBar } from './components/navBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenidos a La Tienda De Comics
         </p>
         <a
           className="App-link"
---
+++ src/components/navBar/NavBar.js
@@ -0,0 +1,23 @@
+import './NavBar.css';
+
+export const NavBar = () => {
+
+    return (
+ <nav>
+
+     <span>Comic Store</span>
+
+     <ul>
+        <li>Home</li>
+        <li>Featured</li>
+        <li>Categories</li>
+        <li>About Us</li>
+    </ul>
+
+
+ </nav>
+    );
+
+
+
+}
