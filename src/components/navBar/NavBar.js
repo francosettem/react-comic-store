5e582d211e0e6328fe86ec4bbd3bcb59a11151ee
Franco Settembrini <fransettembrini@gmail.com>
2021-12-04 10:37:26 -0300

    Styles

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,7 +1,5 @@
 import './NavBar.css';
 import { CartWidget } from '../cartWidget/CartWidget';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { NavLink } from 'react-router-dom';
 
 export const NavBar = () => {
@@ -11,15 +9,11 @@ export const NavBar = () => {
      <NavLink to="/">      
      <span className="navBar__title">Comic Store</span>
      </NavLink>
-     <div className="toggleMenu">
-     <FontAwesomeIcon icon={faBars} />
-     </div>
 
      <ul className="navBar__menu">
          <NavLink to="/">      
           <li className="Menu__item">Home</li>
           </NavLink>
-        <li className="Menu__item">Featured</li>
         <NavLink to="/category/comic">
             <li className="Menu__item">Comic</li>
             </NavLink>
@@ -30,7 +24,6 @@ export const NavBar = () => {
             <li className="Menu__item">Manga</li>
             </NavLink>
         <NavLink to="/category/merchandising"><li className="Menu__item">Merchandising</li></NavLink>
-        <li className="Menu__item">About Us</li>
         <NavLink to="/cart"><CartWidget /></NavLink>
     </ul>      
 
